b64b2383e110f1d8784aa2e40420e9e66816b38f
z008hy <z008hy@163.com>
2018-01-11 22:40:43 +0800

    init app

---
+++ src/components/message.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="message-content">
+    <div class="asker" v-if="type==='asker'">
+      <img src="../assets/asker.jpeg" alt="">
+      <div class="message">
+        {{message}}
+      </div>
+    </div>
+    <div class="answer" v-else>
+      <img src="../assets/answer.jpeg" alt="">
+      <div class="message">{{message}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'message',
+    props: {
+      type: {
+        type: String,
+        default: 'asker'
+      },
+      message: {
+        type: String,
+        default: 'hello! 你好，我是岁月如同马匹！'
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .message-content{
+    display: block;
+    overflow: auto;
+    margin: 20px 0;
+    img{
+      width: 40px;
+      height: 40px;
+    }
+    .message{
+      max-width: 60%;
+      min-height: 20px;
+      line-height: 20px;
+      position: relative;
+      word-wrap: break-word;
+      word-break: break-all;
+      padding: 9px 13px;
+      font-size: 14px;
+      border-radius: 2px;
+    }
+    .asker{
+      overflow: auto;
+      float: right;
+      .message{
+        float: right;
+        background-color: #BCDF8C;
+        margin-right: 10px;
+        &:after{
+          position: absolute;
+          content: "";
+          right: -8px;
+          top: 10px;
+          width: 0;
+          height: 0;
+          border-style: solid;
+          border-width: 5px 0 5px 8px;
+          border-color: transparent transparent transparent #BCDF8C;
+        }
+      }
+      img{
+        float: right;
+      }
+    }
+    .answer{
+      overflow: auto;
+      float: left;
+      .message{
+        float: left;
+        background-color: #FFFFFF;
+        margin-left: 10px;
+        &:after{
+          position: absolute;
+          content: "";
+          left: -8px;
+          top: 10px;
+          width: 0;
+          height: 0;
+          border-style: solid;
+          border-width: 5px 8px 5px 0;
+          border-color: transparent #FFFFFF transparent transparent;
+        }
+      }
+      img{
+        float: left;
+      }
+    }
+  }
+</style>
---
+++ src/page/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="chat">
+    <div class="chat-content">
+      <div class="chat-head">
+        {{user}}
+      </div>
+      <div class="chat-body">
+        <message type="asker"></message>
+        <message type="answer" message="反弹全部反弹！！！！！！！！！！！！！！！！！！！你骂我的，说我的，全部反弹！！！！！！！！"></message>
+
+      </div>
+      <div class="chat-foot"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import message from '../components/message.vue'
+  export default {
+    name: 'chat',
+    components: {
+      message
+    },
+    data () {
+      return {
+        user: '岁月如同马匹',
+        nowTime: '21:36',
+        asker: {
+
+        },
+        answer: {
+
+        },
+        askList: [],
+        answerList: []
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .chat{
+    height: 100%;
+    max-width: 800px;
+    min-width: 600px;
+    margin: 0 auto;
+    .chat-content{
+      position: relative;
+      background-color: #EEEEEE;
+      max-height: 800px;
+      height: 100%;
+      border-radius: 4px;
+      .chat-head{
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        padding: 10px 0;
+        height: 40px;
+        line-height: 40px;
+        border-bottom: 1px solid #d6d6d6;
+        text-align: center;
+      }
+      .chat-body{
+        position: absolute;
+        top: 60px;
+        bottom: 200px;
+        left: 0;
+        right: 0;
+        padding: 10px 20px;
+      }
+      .chat-foot{
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        padding: 10px 0;
+        height: 180px;
+        line-height: 40px;
+        border-top: 1px solid #d6d6d6;
+      }
+    }
+  }
+</style>
